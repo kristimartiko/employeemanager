cb84d81b5ba7f92ca11333821e417e44100dffc5
Kristi <kristi.martiko@fshnstudent.info>
2021-03-16 19:02:50 +0100

    Final

--- employeemanager/src/main/java/com/getarrays/employeemanager/repo/EmployeeRepo.java
+++ employeemanager/src/main/java/com/getarrays/employeemanager/repo/EmployeeRepo.java
@@ -2,10 +2,12 @@ package com.getarrays.employeemanager.repo;
 
 import com.getarrays.employeemanager.model.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.Optional;
 
 public interface EmployeeRepo extends JpaRepository<Employee, Long> {
-    void deleteEmployeeById(Long id);
     Optional<Employee> findEmployeeById(Long id);
 }
--- employeemanager/src/main/java/com/getarrays/employeemanager/service/EmployeeService.java
+++ employeemanager/src/main/java/com/getarrays/employeemanager/service/EmployeeService.java
@@ -45,7 +45,7 @@ public class EmployeeService {
     }
 
     public void deleteEmployee(Long id) {
-        employeeRepo.deleteEmployeeById(id);
+        employeeRepo.deleteById(id);
     }
 
     public Employee findEmployeeById(Long id) {
